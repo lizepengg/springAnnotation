636343d1a08998338ede3c537730600d770b2b7a
lzp <lzp@DESKTOP-GTP0SHQ>
2020-11-28 15:56:29 +0800

    second

--- springTest/src/main/java/com/lzp/beans/Red.java
+++ springTest/src/main/java/com/lzp/beans/Red.java
@@ -1,5 +1,28 @@
 package com.lzp.beans;
 
-public class Red {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component      //添加到ioc容器中
+public class Red implements ApplicationContextAware,BeanNameAware{
+	
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("red类注入ioc容器"+applicationContext);
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("bean的名字为"+name);
+	}
+	
+	
 
 }
